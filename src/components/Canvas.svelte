489bb4883cb38cc2010dd041b25a7bc3503b00c0
Alexey Kondakov <ak.kimpo@gmail.com>
2024-02-08 12:23:16 +0500

    upd: canvas

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { getContext } from 'svelte';
+    import { getContext, onMount } from 'svelte';
     import mergeFloorplan from '../lib/mergeFloorplan.js';
 
     const cfg = getContext('cfg');
@@ -12,7 +12,22 @@
     };
 
     // eslint-disable-next-line no-unused-expressions
-    $: mergeFloorplan(canvas, $floor, $cfg), $floor, $cfg;
-</script>
+    $: if (canvas && $floor && $cfg) {
+      mergeFloorplan(canvas, $floor, $cfg);
+    }
 
-<canvas id="canvas" use:loadImages></canvas>
+    onMount(() => {
+      if (!$floor) {
+        $floor = 'gf';
+      }
+    });
+</script>
+<div class="canvas">
+    <canvas id="canvas" use:loadImages></canvas>
+</div>
+
+<style>
+    /* .canvas {
+
+    } */
+</style>
--- src/lib/mergeFloorplan.js
+++ src/lib/mergeFloorplan.js
@@ -1,38 +1,57 @@
-const getImage = (layer, options) => new Promise((resolve, reject) => {
+const loadLayer = (layer, options) => new Promise((resolve, reject) => {
   const img = new Image();
-  img.crossOrigin = options.crossOrigin;
+  img.crossOrigin = options?.canvas?.crossOrigin;
   img.src = layer.src;
 
   img.onerror = () => reject(new Error('Image loading error'));
   img.onload = () => resolve({ ...layer, img });
 });
 
-const fillCanvas = (canvas, ctx, images, options) => {
-  const firstImage = images[0]?.img;
+const fillCanvas = (canvas, ctx, floor, layers, options) => {
+  const firstImage = layers[0]?.img;
+  const allLayers = options?.floors?.[floor];
 
-  canvas.width = options?.canvas?.width || firstImage?.width;
-  canvas.height = options?.canvas?.height || firstImage?.height;
+  canvas.width = firstImage?.width * 0.2;
+  canvas.height = firstImage?.height * 0.2;
 
-  images.forEach((image) => {
-    ctx.globalAlpha = image.opacity ? image.opacity : options?.floors?.gf?.groupOpacity ?? 1;
-    ctx.drawImage(image.img, image.left || 0, image.top || 0);
+  layers.sort((layer1, layer2) => layer1.zIndex - layer2.zIndex);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  layers.forEach((layer) => {
+    if ((!allLayers?.groupOpacity && allLayers?.groupOpacity !== 0)
+            || options?.floors?.gf?.groupOpacity === 1) {
+      ctx.globalAlpha = layer.opacity || layer.opacity === 0 ? layer.opacity : 1;
+    } else {
+      ctx.globalAlpha = allLayers?.groupOpacity ?? 1;
+    }
+
+    const groupPos = options?.floors?.gf?.left && options?.floors?.gf?.top
+      ? options?.floors?.gf
+      : null;
+
+    ctx.drawImage(
+      layer.img,
+      groupPos?.left || layer?.left || 0,
+      groupPos?.top || layer?.top || 0,
+      canvas.width,
+      canvas.height,
+    );
   });
 
   return canvas.toDataURL(options.format, options.quality);
 };
 
 export default (canvas, floor, options) => new Promise((resolve) => {
-  if (!floor) floor = 'gf';
-
   const { layers } = options.floors[floor];
 
-  const images = layers
-    .map((layer) => getImage(layer, options));
+  const promiseLayers = layers
+    .map((layer) => loadLayer(layer, options));
 
   const ctx = canvas.getContext('2d');
 
   resolve(
-    Promise.all(images)
-      .then((images) => fillCanvas(canvas, ctx, images, options)),
+    Promise.all(promiseLayers)
+      .then((loadedLayers) => fillCanvas(canvas, ctx, floor, loadedLayers, options)),
   );
 });
