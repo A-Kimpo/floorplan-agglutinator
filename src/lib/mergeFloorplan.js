eaab05016569beb87d4b525582eb385c825afe87
Alexey Kondakov <ak.kimpo@gmail.com>
2024-02-08 16:53:08 +0500

    upd: merge func

--- src/lib/mergeFloorplan.js
+++ src/lib/mergeFloorplan.js
@@ -9,33 +9,44 @@ const loadLayer = (layer, options) => new Promise((resolve, reject) => {
 
 const fillCanvas = (canvas, ctx, floor, layers, options) => {
   const firstImage = layers[0]?.img;
-  const allLayers = options?.floors?.[floor];
+  const currentFloor = options?.floors?.[floor];
 
-  canvas.width = firstImage?.width * 0.2;
-  canvas.height = firstImage?.height * 0.2;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.width = options?.canvas?.width || firstImage?.width * 0.2;
+  canvas.height = options?.canvas?.height || firstImage?.height * 0.2;
 
+  // Сортируем слои в зависимости от их z-index
   layers.sort((layer1, layer2) => layer1.zIndex - layer2.zIndex);
 
+  // Очищаем контекст
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  if (options?.canvas?.scale) {
+    ctx.scale(options.canvas.scale, options.canvas.scale);
+  }
+
+  // Рисуем слои в соответствии с опциями из конфига
   layers.forEach((layer) => {
-    if ((!allLayers?.groupOpacity && allLayers?.groupOpacity !== 0)
-            || options?.floors?.gf?.groupOpacity === 1) {
+    if (
+      (!currentFloor?.groupOpacity && currentFloor?.groupOpacity !== 0)
+            || currentFloor?.groupOpacity === 1
+    ) {
       ctx.globalAlpha = layer.opacity || layer.opacity === 0 ? layer.opacity : 1;
     } else {
-      ctx.globalAlpha = allLayers?.groupOpacity ?? 1;
+      ctx.globalAlpha = currentFloor?.groupOpacity ?? 1;
     }
 
-    const groupPos = options?.floors?.gf?.left && options?.floors?.gf?.top
-      ? options?.floors?.gf
+    const floorPosition = currentFloor?.left && currentFloor?.top
+      ? currentFloor
       : null;
 
     ctx.drawImage(
       layer.img,
-      groupPos?.left || layer?.left || 0,
-      groupPos?.top || layer?.top || 0,
-      canvas.width,
-      canvas.height,
+      floorPosition?.left ? layer?.left + floorPosition?.left : layer?.left || 0,
+      floorPosition?.top ? layer?.top + floorPosition?.top : layer?.top || 0,
+      (layer.img?.width || 0) * 0.2 || canvas.width,
+      (layer.img?.height || 0) * 0.2 || canvas.height,
     );
   });
 
@@ -43,14 +54,20 @@ const fillCanvas = (canvas, ctx, floor, layers, options) => {
 };
 
 export default (canvas, floor, options) => new Promise((resolve) => {
+  if (!floor) floor = 'gf';
+
+  // Достаем слои из конфига
   const { layers } = options.floors[floor];
 
+  // Загружаем все слои
   const promiseLayers = layers
     .map((layer) => loadLayer(layer, options));
 
+  // Создаем 2d контекст
   const ctx = canvas.getContext('2d');
 
   resolve(
+    // Ждем fulfilled всех промисов и заполняем canvas слоями
     Promise.all(promiseLayers)
       .then((loadedLayers) => fillCanvas(canvas, ctx, floor, loadedLayers, options)),
   );
